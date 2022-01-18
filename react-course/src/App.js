b3eaec5bf855a102e3ee4a36fa7181c05c8c2664
Yash Kumar Singh <yashkumarsinghpatwa7889@gmail.com>
2022-01-18 21:37:41 +0530

    Added some more styling

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -2,22 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AllMeetup from './pages/AllMeetup';
 import NewMeetup from './pages/NewMeetup';
 import Favourites from './pages/Favourites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
+
 
 function App() {
   return (
-    <div>
 
-      <Router>
-        <MainNavigation />
+
+    <Router>
+      <Layout>
         <Routes>
           <Route exact path='/' element={< AllMeetup />}></Route>
           <Route exact path='/newmeetup' element={< NewMeetup />}></Route>
           <Route exact path='/favourites' element={< Favourites />}></Route>
         </Routes>
+      </Layout>
+    </Router>
 
-      </Router>
-    </div>
   );
 }
 
