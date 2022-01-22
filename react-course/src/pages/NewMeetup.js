7693422203a27572b9246e2fb2f473c0a268ac13
Yash Kumar Singh <yashkumarsinghpatwa7889@gmail.com>
2022-01-22 22:46:46 +0530

    Added NewMeetup From

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetup() {
     return (
-        <div>
-            All NewMeetup Page
-        </div>
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm />
+        </section>
     )
 }
 
